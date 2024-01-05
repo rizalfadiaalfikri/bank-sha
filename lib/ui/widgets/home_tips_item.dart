2b2ebd0286d6c19f197d0221afe0adadae8e46b5
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-01-05 21:31:20 +0700

    add uri launcher in home page

--- lib/ui/widgets/home_tips_item.dart
+++ lib/ui/widgets/home_tips_item.dart
@@ -1,5 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeTipsItem extends StatelessWidget {
   final String imageUrl;
@@ -13,44 +14,51 @@ class HomeTipsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 140,
-      height: 185,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(20),
-            ),
-            child: Image.asset(
-              imageUrl,
-              width: 155,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 12,
-            ),
-            child: Text(
-              title,
-              style: blackTextStyle.copyWith(
-                fontSize: 14,
-                fontWeight: medium,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () async {
+        if (await canLaunchUrl(Uri.parse(url))) {
+          launch(url);
+        }
+      },
+      child: Container(
+        width: 140,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20),
               ),
-              maxLines: 2,
+              child: Image.asset(
+                imageUrl,
+                width: 155,
+                height: 110,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+              ),
+              child: Text(
+                title,
+                style: blackTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: medium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                maxLines: 2,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
