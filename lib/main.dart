d759677fa5cd6e732083c57650d4f9c8393a0b7e
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-01-05 23:54:38 +0700

    slicing pin page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/pages/home_page.dart';
 import 'package:bank_sha/ui/pages/onboarding_page.dart';
+import 'package:bank_sha/ui/pages/pin_page.dart';
 import 'package:bank_sha/ui/pages/profile_page.dart';
 import 'package:bank_sha/ui/pages/signin_page.dart';
 import 'package:bank_sha/ui/pages/signup_page.dart';
@@ -44,6 +45,7 @@ class MyApp extends StatelessWidget {
         '/signup-success': (context) => SignUpSuccessPage(),
         '/home': (context) => HomePage(),
         '/profile': (context) => ProfilePage(),
+        '/pin': (context) => PinPage(),
       },
     );
   }
---
+++ lib/ui/pages/pin_page.dart
@@ -0,0 +1,208 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class PinPage extends StatefulWidget {
+  @override
+  State<PinPage> createState() => _PinPageState();
+}
+
+class _PinPageState extends State<PinPage> {
+  final TextEditingController pinController = TextEditingController(text: '');
+  addPin(String number) {
+    if (pinController.text.length < 6) {
+      setState(() {
+        pinController.text = pinController.text + number;
+      });
+    }
+  }
+
+  deletePin() {
+    if (pinController.text.isNotEmpty) {
+      setState(() {
+        pinController.text =
+            pinController.text.substring(0, pinController.text.length - 1);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sha PIN',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+              const SizedBox(
+                height: 72,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  enabled: false,
+                  controller: pinController,
+                  obscureText: true,
+                  cursorColor: greyColor,
+                  obscuringCharacter: '*',
+                  style: TextStyle(
+                    letterSpacing: 16,
+                    fontSize: 30,
+                    fontWeight: medium,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 66,
+              ),
+              Wrap(
+                spacing: 40,
+                runSpacing: 40,
+                children: [
+                  CustomInputButton(
+                    title: '1',
+                    onTap: () {
+                      addPin('1');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '2',
+                    onTap: () {
+                      addPin('2');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '3',
+                    onTap: () {
+                      addPin('3');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '4',
+                    onTap: () {
+                      addPin('4');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '5',
+                    onTap: () {
+                      addPin('5');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '6',
+                    onTap: () {
+                      addPin('6');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '7',
+                    onTap: () {
+                      addPin('7');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '8',
+                    onTap: () {
+                      addPin('8');
+                    },
+                  ),
+                  CustomInputButton(
+                    title: '9',
+                    onTap: () {
+                      addPin('9');
+                    },
+                  ),
+                  const SizedBox(
+                    width: 60,
+                    height: 60,
+                  ),
+                  CustomInputButton(
+                    title: '0',
+                    onTap: () {
+                      addPin('0');
+                    },
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      deletePin();
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: numberBackgroundColor,
+                      ),
+                      child: Center(
+                          child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      )),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomInputButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onTap;
+  const CustomInputButton(
+      {super.key, required this.title, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: numberBackgroundColor,
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: semiBold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
